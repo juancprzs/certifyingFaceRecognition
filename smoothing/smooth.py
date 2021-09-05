43fb87a9369d68f4f1656b1bebe6a61d559aa55a
MotasemAlfarra <motasem.alfarra@kaust.edu.sa>
2021-09-05 11:03:43 +0300

    minor issue

--- smoothing/smooth.py
+++ smoothing/smooth.py
@@ -2,11 +2,11 @@ import torch
 import torch.nn as nn
 
 class smooth():
-    def __init__(self, sigma) -> None:
+    def __init__(self, model, sigma) -> None:
         pass
 
     def predict(self, x, N):
         return
-        
+
     def certify(self, x, N):
         return
