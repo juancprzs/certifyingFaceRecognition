c9492cd36fb6c7151366b457b83f2359842ea14c
Juan C. Perez Santamaria <perezjc@kw60953.kaust.edu.sa>
2021-09-09 16:25:12 +0300

    debugging

--- launch_jobs.sh
+++ launch_jobs.sh
@@ -2,9 +2,9 @@
 #SBATCH -N 1
 #SBATCH --partition=batch
 #SBATCH --array=[0-24]
-#SBATCH -J adam_xent_lr1e-0
-#SBATCH -o logs/adam_xent_lr1e-0.%J.out
-#SBATCH -e logs/adam_xent_lr1e-0.%J.err
+#SBATCH -J adam_xent_lr1e-1
+#SBATCH -o logs/adam_xent_lr1e-1.%J.out
+#SBATCH -e logs/adam_xent_lr1e-1.%J.err
 #SBATCH --time=4:00:00
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:1
@@ -17,6 +17,6 @@ source venv/bin/activate
 python main_attack.py \
 --chunks 25 \
 --num-chunk ${SLURM_ARRAY_TASK_ID} \
---output-dir adam_xent_lr1e-0 \
---lr 1e-0 \
+--output-dir adam_xent_lr1e-1 \
+--lr 1e-1 \
 --loss xent
