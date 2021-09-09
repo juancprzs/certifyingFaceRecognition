82e574e49030928de84b75ba74203e3999d6d043
Juan C. Perez Santamaria <perezjc@kw60953.kaust.edu.sa>
2021-09-09 11:46:50 +0300

    debugging

--- launch_jobs.sh
+++ launch_jobs.sh
@@ -6,17 +6,18 @@
 #SBATCH -o logs/adam_xent.%J.out
 #SBATCH -e logs/adam_xent.%J.err
 #SBATCH --time=4:00:00
-#SBATCH --mem=50G
+#SBATCH --mem=32G
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-user=perezjc@kaust.edu.sa
 #SBATCH --mail-type=ALL
 
+conda activate upd_pt
 source venv/bin/activate
 
 python main_attack.py \
 --chunks 25 \
 --num-chunk ${SLURM_ARRAY_TASK_ID} \
---output-dir debug_adam_lr1e-0 \
+--output-dir debugging \
 --lr 1e-0 \
 --loss xent
