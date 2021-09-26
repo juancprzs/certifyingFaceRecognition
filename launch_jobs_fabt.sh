ae0affc61ed024045439569b1c8a72bee6f8f00b
Juan C. Perez Santamaria <perezjc@kw60953.kaust.edu.sa>
2021-09-26 09:35:57 +0300

    new batch script for fabt

---
+++ launch_jobs_fabt.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --partition=batch
+#SBATCH --array=[0-249]
+#SBATCH -J fabt_it5_rest5
+#SBATCH -o logs/fabt_it5_rest5.%J.out
+#SBATCH -e logs/fabt_it5_rest5.%J.err
+#SBATCH --time=1:00:00
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-user=perezjc@kaust.edu.sa
+#SBATCH --mail-type=ALL
+#SBATCH --account conf-cvpr-2021.11.23-ghanembs
+
+source venv/bin/activate
+
+python main_attack.py \
+--chunks 250 --num-chunk ${SLURM_ARRAY_TASK_ID} \
+--load-embs --embs-file embs.pth \
+--attack-type fab-t \
+--restarts 5 \
+--iters 5 \
+--n-target-classes 5 \
+--output-dir fabt_it5_rest5
