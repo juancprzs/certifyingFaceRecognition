2f4787cd9a0596eadc3c407d988e5ffb2f778647
Juan C. Perez Santamaria <perezjc@kw60953.kaust.edu.sa>
2021-09-26 09:26:20 +0300

    launch on ibex

--- launch_jobs.sh
+++ launch_jobs.sh
@@ -2,9 +2,9 @@
 #SBATCH -N 1
 #SBATCH --partition=batch
 #SBATCH --array=[0-24]
-#SBATCH -J adam_xent_it5_rest5_lr1e+2
-#SBATCH -o logs/adam_xent_it5_rest5_lr1e+2.%J.out
-#SBATCH -e logs/adam_xent_it5_rest5_lr1e+2.%J.err
+#SBATCH -J adam_xent_it5_rest5_lr1e+1
+#SBATCH -o logs/adam_xent_it5_rest5_lr1e+1.%J.out
+#SBATCH -e logs/adam_xent_it5_rest5_lr1e+1.%J.err
 #SBATCH --time=1:00:00
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:1
@@ -22,5 +22,5 @@ python main_attack.py \
 --loss xent \
 --restarts 5 \
 --iters 5 \
---lr 1e+2 \
---output-dir adam_xent_it5_rest5_lr1e+2
+--lr 1e+1 \
+--output-dir adam_xent_it5_rest5_lr1e+1
