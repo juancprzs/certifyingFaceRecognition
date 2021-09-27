15c347bb991c1b567b9417c82b33a300374bd67e
Juan C. Perez Santamaria <perezjc@login510-22.ibex.kaust.edu.sa>
2021-09-27 08:45:24 +0300

    update bash

--- launch_jobs_fabt.sh
+++ launch_jobs_fabt.sh
@@ -2,10 +2,10 @@
 #SBATCH -N 1
 #SBATCH --partition=batch
 #SBATCH --array=[0-249]
-#SBATCH -J fabt_it5_rest5_targ5
-#SBATCH -o logs/fabt_it5_rest5_targ5.%J.out
-#SBATCH -e logs/fabt_it5_rest5_targ5.%J.err
-#SBATCH --time=1:00:00
+#SBATCH -J fabt_it10_rest10_targ10
+#SBATCH -o logs/fabt_it10_rest10_targ10.%J.out
+#SBATCH -e logs/fabt_it10_rest10_targ10.%J.err
+#SBATCH --time=4:00:00
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=1
@@ -19,7 +19,7 @@ python main_attack.py \
 --chunks 250 --num-chunk ${SLURM_ARRAY_TASK_ID} \
 --load-embs --embs-file embs.pth \
 --attack-type fab-t \
---restarts 5 \
---iters 5 \
---n-target-classes 5 \
---output-dir fabt_it5_rest5_targ5
+--restarts 10 \
+--iters 10 \
+--n-target-classes 10 \
+--output-dir fabt_it10_rest10_targ10
