b9fba49226ffd8b9fae50925059fad1b2d4bcb5e
Juan C. Perez Santamaria <perezjc@kw60953.kaust.edu.sa>
2021-10-09 10:31:35 +0300

    dont scale by magnitude

--- launch_jobs_manual.sh
+++ launch_jobs_manual.sh
@@ -2,10 +2,10 @@
 #SBATCH -N 1
 #SBATCH --partition=batch
 #SBATCH --array=0-249:10
-#SBATCH -J search_adam_diff_lr1e+1
-#SBATCH -o logs/search_adam_diff_lr1e+1.%J.out
-#SBATCH -e logs/search_adam_diff_lr1e+1.%J.err
-#SBATCH --time=0:20:00
+#SBATCH -J search_debug_xent_lr1e+1
+#SBATCH -o logs/search_debug_xent_lr1e+1.%J.out
+#SBATCH -e logs/search_debug_xent_lr1e+1.%J.err
+#SBATCH --time=1:00:00
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=1
@@ -17,7 +17,7 @@ source venv/bin/activate
 
 python main_attack.py \
 --chunks 250 --num-chunk ${SLURM_ARRAY_TASK_ID} \
---load-embs --embs-file embs.pth --iters 20 --restarts 10 \
+--load-embs --embs-file embs.pth --iters 20 --restarts 20 \
 --lr 1e+1 \
---loss diff \
---output-dir search_adam_diff_lr1e+1
+--loss xent \
+--output-dir search_debug_xent_lr1e+1
